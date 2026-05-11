ae96ebbe12640a761e6ceae4b8aabf81f8a563cf
omkarpanchaldev <omkarpanchal.dev@gmail.com>
2026-05-11 22:52:03 +0530

    connect frontend with backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Render live deployment code
+CORS_ALLOWED_ORIGINS = [
+    "https://daily-expense-tracker-frontend-acbe.onrender.com",
+]
